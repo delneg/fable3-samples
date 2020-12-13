510758dacfc932c61c29c6bad946ca96afaa1793
Denis Bobrov <delneg@yandex.ru>
2020-12-13 21:53:34 +0300

    WIP - trying to get App.fs to work with webpack

--- nodejsbundle/src/App.fs
+++ nodejsbundle/src/App.fs
@@ -1,23 +1,45 @@
 module App
 
-open Fable.Core.JsInterop
+//open Fable.SimpleHttp
+open Fable.Core.Extensions
 open Fable.Import
+open Fable.Core.JsInterop
 open Fetch
+//open Fable.Core.JS.Constructors
 
 importSideEffects "isomorphic-fetch"
+printfn "%A" (Fetch.fetch "https://random.dog/woof.json" [])
 
 // we get a json from our fetch request with a url field
 // so we create this type to map the json object
-type PictureInfo = { Url : string }
+//type PictureInfo = { Url : string }
 
 // This function will fetch a random dog url every reload of the page
-let getRandomDogImage url =
-    fetch url [] // use the fetch api to load our resource
-    |> Promise.bind (fun res -> res.text()) // get the resul
-    |> Promise.map (fun txt -> // bind the result to make further operation
-      printfn "Result: %s" txt
-      )
+//let getRandomDogImage url =
+//    let res = fetch url [] // use the fetch api to load our resource
+//    printfn "%A" res
+//    |> Promise.bind (fun res -> res.text()) // get the resul
+//    |> Promise.map (fun txt -> // bind the result to make further operation
+//      printfn "Result: %s" txt
+//      )
+//Http.get "https://random.dog/woof.json"
+//|> Async.StartAsPromise
+////|> Promise.bind (fun (statusCode, responseText)-> res.text()) // get the resul
+//|> Promise.map (fun (statusCode, responseText) -> // bind the result to make further operation
+//  match statusCode with
+//    | 200 -> printfn "Result %s" responseText
+//    | _ -> printfn "Status %d => %s" statusCode responseText
+// )
+//async {
+//    let! (statusCode, responseText) = Http.get "https://random.dog/woof.json"
+//    match statusCode with
+//    | 200 -> printfn "Everything is fine => %s" responseText
+//    | _ -> printfn "Status %d => %s" statusCode responseText
+//} |> Async.StartAsPromise 
 
+//printfn "Hello, World!"
 // start our app!
-getRandomDogImage "https://random.dog/woof.json" |> ignore
+//let res = Node.Https.
+//printfn "%A" (Fetch.fetch "https://random.dog/woof.json" [])
+//getRandomDogImage "https://random.dog/woof.json" |> ignore
 
